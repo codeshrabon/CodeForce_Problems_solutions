4052a07bc244a8254aebbe0e5a6218d39a6ed366
Faysal Ahmed <19101043@uap-bd.edu>
2025-08-28 19:51:39 +0600

    Geometry problem still not working

--- CodeForceProblemSolver/src/Geometry.java
+++ CodeForceProblemSolver/src/Geometry.java
@@ -1,26 +1,21 @@
 import java.util.Scanner;
 
 public class Geometry {
+    //Geometry
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int A,B;
+        int B = sc.nextInt(); 
+        int A = sc.nextInt(); 
+        B = 2*A;
+        double pi = 3.14159265359;
+        double area = (pi * A * A) / 2.0;
 
-        
+        //double redArea = (A * 1.0 * A) * (1.5 - pi / 2.0);
+        double redArea = (B * 1.0 * B) * (3.0 - pi) / 8.0;
 
-        B = sc.nextInt();
-        A = sc.nextInt();
+        System.out.printf("%.9f\n", redArea);
 
-        double rectangleArea = (double) A * B;
-
-        double radius = A / 2.0;
-        double twoCircleArea  = 2 * Math.PI * radius * radius;
-        double triangleAre = (A * A) / 2.0;
-
-        double redArea = rectangleArea - (twoCircleArea+ triangleAre);
-
-        System.out.printf("%.6f", redArea);
-
-        sc.close();
+                sc.close();
     }
 }
